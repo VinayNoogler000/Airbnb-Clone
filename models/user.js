2b0290f23fba3513691b6fca6b8c1856804056d2
VinayNoogler000 <vinaytambey000@gmail.com>
2025-07-07 17:11:38 +0530

    Refactor user schema to include username and password fields with passport-local-mongoose integration

--- models/user.js
+++ models/user.js
@@ -1,21 +1,15 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', userSchema);
 
