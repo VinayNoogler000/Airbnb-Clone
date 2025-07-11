2931dd2566594af5ff6e86b734c564e696bdb80f
VinayNoogler000 <vinaytambey000@gmail.com>
2025-07-11 22:31:04 +0530

    Add route to handle user signup request

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const validateModel = require("../utils/validateModel.js");
 const { userSchema } = require("../schema.js");
 const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 // router.get("/demo", wrapAsync( async (req, res) => {
 //     const newUser = new User ({
@@ -17,10 +18,22 @@ router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", validateModel(userSchema), (req, res) => {
-    const {user} = req.body;
-    
-    res.send(user);
+router.post("/signup", validateModel(userSchema), async (req, res) => {
+    try {
+        const {user} = req.body;
+        const newUser = new User({username: user.name, email: user.email});
+        
+        const registeredUser = await User.register(newUser, user.password);
+        
+        console.log(registeredUser);
+        req.flash("success", `Welcome to Wonderlust, ${user.name}!`);
+        res.redirect("/listings");
+    }
+    catch (err) {
+        console.log(err.message);
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
 });
 
 module.exports = router;
