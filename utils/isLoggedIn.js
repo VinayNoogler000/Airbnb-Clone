990f29aa6fe012abd6292102b489d955ae20892f
VinayNoogler000 <vinaytambey000@gmail.com>
2025-07-12 18:02:47 +0530

    Fix syntax

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -1,6 +1,6 @@
 // Define a Middleware function which returns true, if the user is authenticated, or else false:
 module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticate()) {
+    if (!req.isAuthenticated()) {
         req.flash("error", "You must be logged in to create a new listing!");
         res.redirect("/login"); 
     }
