6852ab63fff1865c692764d2e3340e9491154cdb
VinayNoogler000 <vinaytambey000@gmail.com>
2025-07-15 12:40:07 +0530

    Feat: Authorization middelware for allowing only the owner of the property listings to modify it.

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,4 +1,5 @@
 const ExpressError = require("../utils/ExpressError.js");
+const Listing = require("../models/listing");
 
 // Define a Middleware function which returns true, if the user is authenticated, or else false:
 module.exports.isLoggedIn = (req, res, next) => {
@@ -10,14 +11,13 @@ module.exports.isLoggedIn = (req, res, next) => {
     else next();
 }
 
-// define a Middleware func() to save the URL as a local variable, to which the client send request to:
+// define a Middleware func() to save the originalURL as a local variable, to which the client sent request to:
 module.exports.saveRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 }
-
 
 // define a Middleware factory func() to Validate Model Data, sent by the client:
 module.exports.validateModel = (schema) => (req, res, next) => {
@@ -30,3 +30,16 @@ module.exports.validateModel = (schema) => (req, res, next) => {
         return next();
     }
 };
+
+// define a Middlelware to check whether the user is authorized to Update or Delete a Listing/Review or not:
+module.exports.isAuthorized = async (req, res, next) => {
+    const {id} = req.params; // listing_id
+    const currUser = res.locals.currUser;
+    const listing = await Listing.findById(id);
+
+    if (!currUser._id.equals(listing.owner._id)) {
+        req.flash("error", "You're not allowed to modify this listing!");
+        res.redirect(`/listings/${id}`);
+    }
+    else next();
+}
