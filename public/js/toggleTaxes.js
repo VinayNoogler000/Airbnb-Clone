c2efccf662b2ae30f4528c738fd1d442cbd649c9
VinayNoogler000 <vinaytambey000@gmail.com>
2025-07-23 21:08:15 +0530

    feat: Add client-side tax toggle for listing prices
    This commit introduces a new feature allowing users to view listing prices with an 18% tax included. A toggle switch has been added to
    the filter bar for this purpose.
    
    Key changes:
     - `views/includes/filter.ejs`: Added a Bootstrap form switch to the filter component to control the tax display.
     - `public/js/toggleTaxes.js`: New JavaScript file containing the logic to calculate and dynamically update the prices on the index page
       when the toggle is switched.
     - `views/listings/index.ejs`: Wrapped the price in a <span> to allow for easier selection and manipulation by the new script.
     - `public/css/filter.css`: Added styling for the new tax toggle switch and adjusted existing filter styles.

---
+++ public/js/toggleTaxes.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => { 
+    const taxToggleEl = document.querySelector("#switchCheckDefault");
+    
+    taxToggleEl.addEventListener("click", () => {
+        const allListingsPrices = document.querySelectorAll(".listings .card .price");
+        
+        allListingsPrices.forEach((priceEl) => {
+            let actualPrice = priceEl.actualPrice ? priceEl.actualPrice : Number(priceEl.textContent.split(',').join('')); // excl. 18% GST
+            
+            if(taxToggleEl.checked === true) { // show price with tax
+                let totalPrice = priceEl.totalPrice ? priceEl.totalPrice : (actualPrice * 0.18) + actualPrice; // incl. 18% GST
+                priceEl.textContent = totalPrice.toLocaleString("en-IN");
+                if (!priceEl.actualPrice && !priceEl.totalPrice) {
+                    priceEl.actualPrice = actualPrice;
+                    priceEl.totalPrice = totalPrice;
+                }
+            }
+            else { // show price without tax
+                priceEl.textContent = actualPrice.toLocaleString("en-IN");
+            }
+        });
+    });
+});
