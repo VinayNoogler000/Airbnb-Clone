372e03ea901c3bfe4f5f0e2002b8cf2218821b4d
VinayNoogler000 <vinaytambey000@gmail.com>
2025-07-26 22:43:31 +0530

    feat(navbar): Add tax toggle switch for mobile view
    This adds a new tax toggle switch directly into the navbar, making it easily accessible on mobile devices.
    
    To support multiple toggles on the same page, the `toggleTaxes.js` script has been refactored to use a class-based selector instead
    of a unique ID. This ensures that both the existing filter toggle and the new navbar toggle function correctly.

--- public/js/toggleTaxes.js
+++ public/js/toggleTaxes.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => { 
-    const taxToggleEl = document.querySelector("#switchCheckDefault");
+    const taxToggleEls = document.querySelectorAll(".form-check-input");
     
-    taxToggleEl.addEventListener("click", () => {
-        const allListingsPrices = document.querySelectorAll(".listings .card .price");
-        
-        allListingsPrices.forEach((priceEl) => {
-            let actualPrice = priceEl.actualPrice ? priceEl.actualPrice : Number(priceEl.textContent.split(',').join('')); // excl. 18% GST
+    taxToggleEls.forEach((taxToggleEl) => {
+        taxToggleEl.addEventListener("click", () => {
+            const allListingsPrices = document.querySelectorAll(".listings .card .price");
             
-            if(taxToggleEl.checked === true) { // show price with tax
-                let totalPrice = priceEl.totalPrice ? priceEl.totalPrice : (actualPrice * 0.18) + actualPrice; // incl. 18% GST
-                priceEl.textContent = totalPrice.toLocaleString("en-IN");
-                if (!priceEl.actualPrice && !priceEl.totalPrice) {
-                    priceEl.actualPrice = actualPrice;
-                    priceEl.totalPrice = totalPrice;
+            allListingsPrices.forEach((priceEl) => {
+                let actualPrice = priceEl.actualPrice ? priceEl.actualPrice : Number(priceEl.textContent.split(',').join('')); // excl. 18% GST
+                
+                if(taxToggleEl.checked === true) { // show price with tax
+                    let totalPrice = priceEl.totalPrice ? priceEl.totalPrice : (actualPrice * 0.18) + actualPrice; // incl. 18% GST
+                    priceEl.textContent = totalPrice.toLocaleString("en-IN");
+                    if (!priceEl.actualPrice && !priceEl.totalPrice) {
+                        priceEl.actualPrice = actualPrice;
+                        priceEl.totalPrice = totalPrice;
+                    }
                 }
-            }
-            else { // show price without tax
-                priceEl.textContent = actualPrice.toLocaleString("en-IN");
-            }
+                else { // show price without tax
+                    priceEl.textContent = actualPrice.toLocaleString("en-IN");
+                }
+            });
         });
-    });
+    }); 
 });
